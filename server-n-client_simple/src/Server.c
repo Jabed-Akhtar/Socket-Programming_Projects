865c07b3ae57282a430bbda86dded234d5747844
Jabed-Akhtar <jabed.akhtar@outlook.com>
2022-09-22 19:11:03 +0200

    new example server_n_client_simple added

--- server-n-client_simple/src/Server.c
+++ server-n-client_simple/src/Server.c
@@ -1,58 +1,59 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <unistd.h>
-#include <sys/types.h>
-#include <sys/socket.h>
-#include <netinet/in.h>
-
-void error(const char *msg);
-
-int main(int argc, char *argv[]){
-    if(argc < 2){
-        fprintf(stderr, "Port No. not provided. Programm terminated.\n");
-        exit(1);
-    }
-
-    int sockfd, newsockfd, portno, n;
-    char buffer[255];
-
-    struct sockaddr_in serv_addr, cli_addr;
-    socklen_t clilen;
-    
-    sockfd = socket(AF_INET, SOCK_STREAM, 0);
-    if(sockfd < 0){
-        error("Error opening Socket.");
-    }
-
-    bzero((char *) &serv_addr, sizeof(serv_addr));
-    portno = atoi(argv[1]); // atoi -> convers string to integer
-
-    serv_addr.sin_family = AF_INET;
-    serv_addr.sin_addr.s_addr = INADDR_ANY;
-    serv_addr.sin_port = htons(portno); // htons -> host to network short
-
-    if(bind(sockfd, (struct sockaddr *) &serv_addr, sizeof(serv_addr)) < 0)
-        error("Binding Failed.");
-
-    listen(sockfd, 5);
-    clilen = sizeof(cli_addr);
-
-    newsockfd = accept(sockfd, (struct sockaddr *) &cli_addr, &clilen);
-
-    if(newsockfd < 0)
-        error("Error on Accept.");
-
-    while(1){
-        bzero(buffer, 256);
-        n = read(newsockfd, buffer, 255);
-        if(n<0)
-            error("Error on reading.");
-        printf("Client: %s\n", buffer);
-    }
-}
-
-void error(const char *msg){
-    perror(msg);
-    exit(1);
+// Server side C/C++ program to demonstrate Socket programming 
+#include <unistd.h> 
+#include <stdio.h> 
+#include <sys/socket.h> 
+#include <stdlib.h> 
+#include <netinet/in.h> 
+#include <string.h> 
+#define PORT 8080 
+int main(int argc, char const *argv[]) 
+{ 
+    int server_fd, new_socket, valread; 
+    struct sockaddr_in address; 
+    int opt = 1; 
+    int addrlen = sizeof(address); 
+    char buffer[1024] = {0}; 
+    char *hello = "Hello from server"; 
+       
+    // Creating socket file descriptor 
+    if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0) 
+    { 
+        perror("socket failed"); 
+        exit(EXIT_FAILURE); 
+    } 
+       
+    // Forcefully attaching socket to the port 8080 
+    if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, 
+                                                  &opt, sizeof(opt))) 
+    { 
+        perror("setsockopt"); 
+        exit(EXIT_FAILURE); 
+    } 
+    address.sin_family = AF_INET; 
+    address.sin_addr.s_addr = INADDR_ANY; 
+    address.sin_port = htons( PORT ); 
+       
+    // Forcefully attaching socket to the port 8080 
+    if (bind(server_fd, (struct sockaddr *)&address,  
+                                 sizeof(address))<0) 
+    { 
+        perror("bind failed"); 
+        exit(EXIT_FAILURE); 
+    } 
+    if (listen(server_fd, 3) < 0) 
+    { 
+        perror("listen"); 
+        exit(EXIT_FAILURE); 
+    } 
+    if ((new_socket = accept(server_fd, (struct sockaddr *)&address,  
+                       (socklen_t*)&addrlen))<0) 
+    { 
+        perror("accept"); 
+        exit(EXIT_FAILURE); 
+    } 
+    valread = read( new_socket , buffer, 1024); 
+    printf("%s",buffer ); 
+    send(new_socket , hello , strlen(hello) , 0 ); 
+    printf("Hello message sent"); 
+    return 0; 
 }
